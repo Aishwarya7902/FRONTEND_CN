fca5a46e1603820f60ec8b24b197743b1e02ec78
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-12-02 13:43:37 +0530

    Add files via upload

---
+++ REACT COMPONENTS 1/User Profile/src/Skills.js
@@ -0,0 +1,17 @@
+import {Component} from "react";
+
+class Skills extends Component{
+    render(){
+        return(
+                <ul >
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>Javascript</li>
+                    <li>React</li>
+                    <li>Node</li>
+                </ul>
+        );
+    }
+}
+
+export default Skills;
